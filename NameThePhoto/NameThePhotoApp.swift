e716b17ac8ae711e44c9f4bddb868d1d86719ad2
Víctor Ávila <victoraavila2000@gmail.com>
2024-08-04 23:39:31 -0300

    Initial Commit

---
+++ NameThePhoto/NameThePhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameThePhotoApp.swift
+//  NameThePhoto
+//
+//  Created by Víctor Ávila on 04/08/24.
+//
+
+import SwiftUI
+
+@main
+struct NameThePhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
